2e6768131c60d9a0d9086563e3366c95de540f83
Slysparrow88 <pavlusha198806@ggmail.com>
2019-10-04 21:21:47 +0300

    creatOOP

--- cart.js
+++ cart.js
@@ -1,4 +1,4 @@
-const ar = [
+const products = [
     {id:1, name: 'product-1'},
     {id:2, name: 'product-2'},
     {id:3, name: 'product-3'},
@@ -7,28 +7,43 @@ const ar = [
     {id:6, name: 'product-6'},
     ] 
 
-    const container = document.querySelector('#products');
-    const cart = document.querySelector('#cart');
+   
+    
 
-    ar.forEach(product => {
+
+const productManager = {
+    products: [],
+    cart: {},
+    container: {},
+
+     createCartProduct: function (event) {
+
+            console.log(event.target);
+
+        const id = +event.currentTarget.dataset.productId;
+        const product = products.find(p =>p.id === id);
+
+        const wrap = document.createElement('div');
+        wrap.className = 'cart-product';
+
+        const h2 = document.createElement('h2');
+        h2.innerHTML = product.name;
+
+        wrap.appendChild(h2);
+        this.cart.appendChild(wrap);
+
+     },
+
+    render: function () {
+        const create = this.createCartProduct.bind(this);
+    for (let product of this.products) {
+        //products.forEach(product =>  {
         
         const wrap = document.createElement('div');
         wrap.className = 'product';
 
-        wrap.onclick = function () {
-           const id = +this.dataset.productId;
-           const product = ar.find(p =>p.id === id);
+        wrap.onclick = create;
 
-           const wrap = document.createElement('div');
-           wrap.className = 'cart-product';
-
-           const h2 = document.createElement('h2');
-           h2.innerHTML = product.name;
-
-           wrap.appendChild(h2);
-           cart.appendChild(wrap);
-   
-        }
         wrap.dataset.productId = product.id;
 
         const h2 = document.createElement('h2');
@@ -36,11 +51,28 @@ const ar = [
 
         wrap.appendChild(h2);
 
-        container.appendChild(wrap);
+        this.container.appendChild(wrap);
+
+            
+    }
+    }
+}
+
+const container = document.querySelector('#products');
+const cart = document.querySelector('#cart');
+
+productManager.container = document.querySelector('#products');
+productManager.cart = document.querySelector('#cart');
+productManager.products = products;
+
+productManager.render();
+
+/*function view() {
+    alert("hi")
+    console.log(this);
+}
+view();
 
 
-    });
-
-    
-
-
+const cont = document.querySelector('#my');
+cont.onclick = view;*/
